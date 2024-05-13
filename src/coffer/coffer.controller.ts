59fa4cc86fc282f9f91bbf8d4f02d06616d1ae48
potato1205 <915237695@qq.com>
2024-05-13 17:06:16 +0800

    add param

--- src/coffer/coffer.controller.ts
+++ src/coffer/coffer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffer')
 export class CofferController {
@@ -12,5 +12,17 @@ export class CofferController {
     findOne(@Param('id') id: string) {
         // 查询单个coffer信息
         return `This action returns a #${id} coffer`;
+    }
+
+    @Post('create')
+    create(@Body() body) {
+        // 创建coffer信息
+        return 'This action adds a new coffer'+ body.name;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        // 更新coffer信息
+        return `This action updates a #${id} coffer`;
     }
 }
