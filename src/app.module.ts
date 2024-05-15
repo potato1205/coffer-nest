72c3b6bc5f8743623feaf1d2f0d85b2e2986ff45
potato1205 <915237695@qq.com>
2024-05-15 17:38:28 +0800

    ../

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CofferController } from './coffer/coffer.controller';
+import { CofferService } from './coffer/coffer.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CofferController],
-  providers: [AppService],
+  providers: [AppService, CofferService],
 })
 export class AppModule {}
